bf4cf8dfe353fe7f9a0e99aea4a21330f65a0567
Noe <Noe.BERDOZ@cpnv.ch>
2020-09-04 19:19:53 +0200

    Preparing git flow

--- components/login.js
+++ components/login.js
@@ -1,9 +1,1 @@
-
 // Tu es entrain d'essayer de mettre le login sur app.js
-import React from 'react'
-import {Button, Text, View} from 'react-native';
-
-
-class Login extends React.Component {
-
-export default Login
