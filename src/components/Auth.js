99e2dbc1abad8c5614f41afc5b5aafe1af38ccc4
Noe <Noe.BERDOZ@cpnv.ch>
2020-09-24 18:23:28 +0200

    Navbar, bottomtab navigation implementation

---
+++ src/components/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Register from './Register';
+
+export default function Auth() {
+    return (
+        <View>
+            <Register/>
+        </View>
+    )
+}
