d0e39cbba2dffacc5dee9db8d13fd74b7819f436
NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>
2020-11-09 11:28:11 +0100

    Post request for new stock

--- src/services/StockerContainer.js
+++ src/services/StockerContainer.js
@@ -5,16 +5,39 @@ import {Map} from 'immutable';
 export const useStockerContainer = (UserContainer) => {
     return () => {
         const userContainer = UserContainer.useContainer();
-        const [stocker, setStocker] = useState(Map)
+        const [stocker, setStocker] = useState(Map());
 
         const axios = require('axios')
 
         const addToStocker = (product, quantity) => {
-            setStocker(stocker.set(product.id, quantity))
-            console.log(stocker)
+            setStocker(stocker.set(product, quantity))
+            console.log("addToStocker: "+stocker)
 
         }
 
-        return {stocker, addToStocker}
+        async function postStockerToApi(values: any) {
+            console.log("PUSHING: "+stocker)
+            axios.post('api/products/stock', {
+                quantities: values.map((value) => {
+                    return {
+                        id: value.id,
+                        quantity: value.quantity
+                    }
+                })
+            },
+            {headers: {
+                Authorization: "Bearer " + userContainer.token
+            }}
+        )
+            .then(function (response: any) {
+                console.log(response)
+                setStocker(Map())
+            })
+                .catch(function (error: any) {
+                    console.log(error)
+                })
+        }
+
+        return {stocker, addToStocker, postStockerToApi}
     }
 }
